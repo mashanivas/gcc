9668bafe3a3f2ce0ea9906aaa4e62e951d9edd25
mashanivas <mashanivas@gmail.com>
2021-02-10 14:38:36 -0600

    Initial committ

---
+++ source/c3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+
+   char characterName[] = "Suresh";
+   int characterAge = 55;
+   printf("The name of the person is %s\n", characterName);
+   printf("The age of the person is %d\n", characterAge);
+   return 0;
+
+}
---
+++ source/calc1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+
+   double int1;
+   double int2;
+   printf("Enter first number:\n ");
+   scanf("%lf", &int1);
+   printf("Enter second number:\n ");
+   scanf("%lf", &int2);
+   printf("The sum is %f\n :", int1 + int2);
+   return 0;
+
+}
